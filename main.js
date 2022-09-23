93f91f5aae02a3aa8ef8dc3c21f33022c9bf2856
Sampalmer8 <sampalmerg@gmail.com>
2022-09-23 10:53:07 -0600

    dropdown working and finished

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 // JavaScript Code
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+// QUIZ JAVASCRIPT
+// QUIZ JAVASCRIPT
+// QUIZ JAVASCRIPT
+// QUIZ JAVASCRIPT
