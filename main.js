a0766cdd1b2e0aa93b1d27fad38022590d6b8b12
Sampalmer8 <sampalmerg@gmail.com>
2022-10-06 10:52:55 -0600

    FINISHED QUIZ

--- main.js
+++ main.js
@@ -23,20 +23,33 @@ function myFunction() {
 // QUIZ JAVASCRIPT
 document.getElementById("btn").addEventListener("click", btnClicked);
 
-
+let score = 0;
 
 function btnClicked() {
-  // Input
-  let question1 = document.getElementById("answer1").toLowerCase();
-  let question2 = document.getElementById("answer2").toLowerCase();
-  let question3 = document.getElementById("answer3").toLowerCase();
-  let question4 = document.getElementById("answer4").toLowerCase();
-  let question5 = document.getElementById("answer5").toLowerCase();
-
+  score = 0;
+  let question1 = document.getElementById("answer1").value.toLowerCase();
+  let question2 = document.getElementById("answer2").value.toLowerCase();
+  let question3 = document.getElementById("answer3").value.toLowerCase();
+  let question4 = document.getElementById("answer4").value.toLowerCase();
+  let question5 = document.getElementById("answer5").value.toLowerCase();
   // If statements
-
-  // Process
-
+  if (question1 === "cpu" || question1 === "processor" || question1 === "central processing unit") {
+    score++;
+  }
+  if (question2 === "nvidia") {
+    score++;
+  }
+  if (question3 === "random access memory") {
+    score++;
+  }
+  if (question4 === "motherboard") {
+    score++;
+  }
+  if (question5 === "ghz" || question5 === "gigahertz") {
+    score++;
+  }
 
   // Output
+  document.getElementById("score").innerHTML = score;
+  document.getElementById("percentage").innerHTML = score/5 * 100;
 }
